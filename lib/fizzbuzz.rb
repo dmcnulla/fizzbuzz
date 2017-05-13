b81ee10417bef3317d66b8492bff54c4e65e7302
Dave McNulla <mcnulla@gmail.com>
2017-05-13 15:37:42 -0700

    Fized buz in the class.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -4,18 +4,14 @@ class FizzBuzz
 	end
 
 	def next(n = 1)
-		msg = ''
-		n.times do
-			@current += 1
-			msg += "#{fizzbuzz(@current)}\n"
-		end
-		msg
+		@current += 1
+		"#{fizzbuzz(@current)}"
 	end
 
 	def fizzbuzz(x)
 		msg = ''
-		msg += 'Fizz' if x%3==0
-		msg += 'Buzz' if x%5==0
+		msg = msg + 'Fizz' if x%3==0
+		msg = msg + 'Buzz' if x%5==0
 		(msg.length > 0) ? msg : x
 	end
 end
